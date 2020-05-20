986deedf951e21cd3e269f7ce1ba891e16e4d721
rahulgurnani <rahulgurnani09@gmail.com>
2020-05-20 21:24:09 +0530

    Fix imread usage for latest version of scipy

--- load_data.py
+++ load_data.py
@@ -1,6 +1,6 @@
 import sys
 import numpy as np
-from scipy.misc import imread
+from imageio import imread
 import pickle
 import os
 import matplotlib.pyplot as plt
@@ -9,35 +9,33 @@ import matplotlib.pyplot as plt
     to index my character type"""
 
 data_path = os.path.join('data/')
-train_folder = os.path.join(data_path,'images_background')
-valpath = os.path.join(data_path,'images_evaluation')
+train_folder = os.path.join(data_path, 'images_background')
+valpath = os.path.join(data_path, 'images_evaluation')
 
 save_path = 'data/'
 
 lang_dict = {}
 
-
-
-def loadimgs(path,n=0):
-    #if data not already unzipped, unzip it.
+def loadimgs(path, n=0):
+    # if data not already unzipped, unzip it.
     if not os.path.exists(path):
         print("unzipping")
         os.chdir(data_path)
-        os.system("unzip {}".format(path+".zip" ))
-    X=[]
+        os.system("unzip {}".format(path+".zip"))
+    X = []
     y = []
     cat_dict = {}
     lang_dict = {}
     curr_y = n
-    #we load every alphabet seperately so we can isolate them later
+    # we load every alphabet seperately so we can isolate them later
     for alphabet in os.listdir(path):
         print("loading alphabet: " + alphabet)
-        lang_dict[alphabet] = [curr_y,None]
-        alphabet_path = os.path.join(path,alphabet)
-        #every letter/category has it's own column in the array, so  load seperately
+        lang_dict[alphabet] = [curr_y, None]
+        alphabet_path = os.path.join(path, alphabet)
+        # every letter/category has it's own column in the array, so  load seperately
         for letter in os.listdir(alphabet_path):
             cat_dict[curr_y] = (alphabet, letter)
-            category_images=[]
+            category_images = []
             letter_path = os.path.join(alphabet_path, letter)
             for filename in os.listdir(letter_path):
                 image_path = os.path.join(letter_path, filename)
@@ -46,7 +44,7 @@ def loadimgs(path,n=0):
                 y.append(curr_y)
             try:
                 X.append(np.stack(category_images))
-            #edge case  - last one
+            # edge case  - last one
             except ValueError as e:
                 print(e)
                 print("error - category_images:", category_images)
@@ -54,15 +52,16 @@ def loadimgs(path,n=0):
             lang_dict[alphabet][1] = curr_y - 1
     y = np.vstack(y)
     X = np.stack(X)
-    return X,y,lang_dict
-
-X,y,c=loadimgs(train_folder)
+    return X, y, lang_dict
 
 
-with open(os.path.join(save_path,"train.pickle"), "wb") as f:
-	pickle.dump((X,c),f)
+X, y, c = loadimgs(train_folder)
 
 
-X,y,c=loadimgs(valpath)
-with open(os.path.join(save_path,"val.pickle"), "wb") as f:
-	pickle.dump((X,c),f)
+with open(os.path.join(save_path, "train.pickle"), "wb") as f:
+    pickle.dump((X, c), f)
+
+
+X, y, c = loadimgs(valpath)
+with open(os.path.join(save_path, "val.pickle"), "wb") as f:
+    pickle.dump((X, c), f)
